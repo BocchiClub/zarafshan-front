fd33daeb66257118b3d0f59142e1a9d0c02a97e6
Darina Rustamova <darisha6602@gmail.com>
2023-10-17 23:36:55 +0300

    Add js csv file

--- js/main.js
+++ js/main.js
@@ -33,7 +33,45 @@ function initSwiper() {
         },
     });
 }
-
+// Функция для загрузки и обработки CSV-файла
+function loadCSV() {
+    Papa.parse("news.csv", {
+      header: true,
+      dynamicTyping: true,
+      download: true,
+      complete: function (results) {
+        if (results.data && results.data.length > 0) {
+          results.data.forEach((news) => {
+            const newsItem = document.createElement("div");
+            newsItem.className = "news-item";
+  
+            const dateElement = document.createElement("p");
+            dateElement.textContent = "Дата: " + news.Дата;
+  
+            const descriptionElement = document.createElement("h3");
+            const newsLink = document.createElement("a");
+            newsLink.href = news.Ссылка;
+            newsLink.textContent = news.Описание;
+            descriptionElement.appendChild(newsLink);
+  
+            newsItem.appendChild(descriptionElement);
+            newsItem.appendChild(dateElement);
+  
+            if (news.Категория === "Органы власти") {
+              const organsColumn = document.getElementById("organs");
+              organsColumn.appendChild(newsItem);
+            } else if (news.Категория === "Общество") {
+              const governorColumn = document.getElementById("society");
+              governorColumn.appendChild(newsItem);
+            }
+          });
+        }
+      },
+    });
+  }
+  
+  // Запустите загрузку CSV-файла и создание новостей
+loadCSV();  
 initSwiper();
 
 // Обновляем Swiper при изменении размера окна
