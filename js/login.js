4d39ab912c0660904114b0d55f13d2c5221e4243
Darina Rustamova <darisha6602@gmail.com>
2023-12-05 22:02:43 +0300

    Add Sign in page

--- js/login.js
+++ js/login.js
@@ -1,29 +1,61 @@
 function submitForm() {
-    var formData = new FormData(document.getElementById('loginForm'));
+  var formData = new FormData(document.getElementById('loginForm'));
 
-    fetch('http://127.0.0.1:5000/token', {
-        method: 'POST',
-        body: formData
-    })
+  fetch('http://127.0.0.1:5000/token', {
+    method: 'POST',
+    body: formData
+  })
     .then(response => {
       if (response.status == 200) {
-        // Successful authorization
-    
-        // Get the username value from the response
+
         var username = formData.get("username");
-    
-        // Store the username in local storage
+
         localStorage.setItem('username', username);
-        // console.log(username);
-        // Redirect to the main page
+
         window.location.href = 'index.html';
       } else {
-        // Authorization error, display a message
         document.getElementById('errorMessage').textContent = 'Неверный логин или пароль';
       }
     })
     .then(data => console.log(data))
     .catch(error => {
-        console.error('Ошибка при отправке запроса:', error);
+      console.error('Ошибка при отправке запроса:', error);
+    });
+}
+
+function signinForm() {
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const fullName = document.getElementById("full_name").value;
+  const password = document.getElementById("password").value;
+
+  const userData = {
+    username: username,
+    email: email,
+    full_name: fullName,
+    password: password
+  };
+
+  fetch("http://127.0.0.1:5000/users/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(userData)
+  })
+    .then(response => {
+      if (response.status == 200) {
+        window.location.href = 'index.html';
+
+        localStorage.setItem('username', username);
+      } else {
+        document.getElementById('errorMessage').textContent = 'Пользователь с таким username уже существует';
+      }
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Ошибка при отправке запроса:', error);
     });
 }
