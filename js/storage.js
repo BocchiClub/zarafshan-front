955e10a0ce308fb6c36e49ec14088be21a233b35
Maxim Fedotov <mvfedotov_1@edu.hse.ru>
2023-12-09 00:28:20 +0300

    add logout button

--- js/storage.js
+++ js/storage.js
@@ -1,3 +1,12 @@
+function logout() {
+  // Delete the user's authentication token (example: using localStorage)
+  localStorage.removeItem('token');
+  localStorage.removeItem('username');
+  document.getElementById('loginLink').textContent = 'Войти';
+  // Redirect the user to the login page or any other desired page
+  window.location.href = 'index.html';
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     // Retrieve the stored username from local storage
     var username = localStorage.getItem('username');
@@ -5,5 +14,13 @@ document.addEventListener('DOMContentLoaded', function() {
     // Update the text content of the login link with the username
     if (username) {
       document.getElementById('loginLink').textContent = username;
+      var listItem = document.createElement('li');
+      var logoutButton = document.createElement('a');
+      logoutButton.id = 'logoutButton';
+      logoutButton.className = 'main-item';
+      logoutButton.textContent = 'Выйти';
+      logoutButton.addEventListener('click', logout);
+      listItem.appendChild(logoutButton);
+      document.getElementById('main-menu').appendChild(listItem);
     }
   });
