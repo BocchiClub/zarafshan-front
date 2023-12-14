40d0d0d9fd8197d7f26bec6117934293a371dc79
Maxim Fedotov <mvfedotov_1@edu.hse.ru>
2023-12-14 23:20:56 +0300

    Add response form

--- js/request_user.js
+++ js/request_user.js
@@ -28,6 +28,19 @@ function sendrequest() {
 }
 
 function getRequests() {
+  let isAdmin;
+  fetch('http://127.0.0.1:5000/users/me/', {
+    headers: {
+      'Authorization': 'Bearer ' + localStorage.getItem('token'),
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    isAdmin = data.admin
+  })
   fetch('http://127.0.0.1:5000/users/me/items', {
     headers: {
       'Authorization': 'Bearer ' + localStorage.getItem('token'),
@@ -50,6 +63,11 @@ function getRequests() {
         var requestInfo = document.createElement('div');
         requestInfo.className = 'request-info';
 
+        if (isAdmin) {
+          var username = document.createElement('p');
+          username.textContent = "Пользователь: " + request.username;
+          requestInfo.appendChild(username);
+        }
         var message = document.createElement('p');
         message.textContent = "Сообщение: " + request.message;
         requestInfo.appendChild(message);
@@ -75,6 +93,14 @@ function getRequests() {
         requestInfo.appendChild(response);
 
         requestsDiv.appendChild(requestInfo);
+        if (isAdmin) {
+          var addResponseBtn = document.createElement('button');
+          addResponseBtn.textContent = 'Добавить ответ';
+          addResponseBtn.addEventListener('click', function() {
+            addResponseForm(request.id, requestInfo);
+          });
+          requestInfo.appendChild(addResponseBtn);
+}
       });
 
       pageWrapper.appendChild(requestsDiv);
@@ -88,4 +114,48 @@ document.addEventListener('DOMContentLoaded', function () {
   getRequests();
 });
 
+function addResponseForm(requestId, requestInfo) {
+  var responseFormWrapper = document.createElement('div');
+  responseFormWrapper.className = 'response-form-wrapper';
 
+  var responseForm = document.createElement('form');
+  responseForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    var responseInput = responseForm.querySelector('#response-input');
+    var response = responseInput.value;
+    sendResponse(requestId, response);
+    responseFormWrapper.remove();
+  });
+
+  var responseInput = document.createElement('input');
+  responseInput.id = 'response-input';
+  responseInput.type = 'text';
+  responseInput.placeholder = 'Введите ответ...';
+  responseForm.appendChild(responseInput);
+
+  var sendResponseBtn = document.createElement('button');
+  sendResponseBtn.textContent = 'Отправить';
+  responseForm.appendChild(sendResponseBtn);
+
+  responseFormWrapper.appendChild(responseForm);
+  requestInfo.appendChild(responseFormWrapper);
+}
+
+function sendResponse(requestId, response) {
+  // Здесь вызываете ручку '/user_reqs/{id_request}/response/' с использованием Fetch API или другой подходящей библиотеки для сетевых запросов.
+  // Пример использования Fetch API:
+  var params = new URLSearchParams();
+  params.append('response', response);
+  fetch(`http://localhost:5000/user_reqs/${requestId}/response/?` + params.toString(), {
+    method: 'PUT',
+    // body: params.toString,
+    // headers: {
+    //   'Content-Type': 'application/json'
+    // }
+  })
+  .then(function(response) {
+  })
+  .catch(function(error) {
+    // Обработка ошибки
+  });
+}
